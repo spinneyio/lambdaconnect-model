ae29cfe506c9cbd69ba24bfb63e694a6166bb233
Uiskam <maksulima@gmail.com>
2022-12-11 15:14:18 +0100

    relevant tag test

---
+++ test/lambdaconnect_model/utils_test.clj
@@ -0,0 +1,11 @@
+(ns lambdaconnect-model.utils-test
+  (:require [clojure.test :refer [deftest is testing]] 
+            [lambdaconnect-model.core :as mp] 
+            [clojure.pprint :as pprint :refer [pprint]]
+            [lambdaconnect-model.utils :refer [relevant-tags]]))
+
+(deftest relevant-tags-test
+  (let [contraint-to-be-tested '(and [= :tmp-field :tag0] (or [= :tmp-field :tag1] (and (or-join [= :tmp-field  :tag4] [= :tmp-field :tag2]) (not [= :tmp-field :tag3])))) 
+        expected-results (reduce (fn [cur-set new-idx] (conj cur-set (keyword (str "tag" new-idx)))) #{} (range 5))]
+    (is (= (relevant-tags contraint-to-be-tested) expected-results))
+    (is (= (relevant-tags :all) #{}))))
